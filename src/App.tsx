8f46038452054a9218bb6d4ed1e76a3a50cfcb4b
Cole Gannaway <cole.gannaway.personal@gmail.com>
2023-06-22 14:33:40 -0500

    chat app as a basis

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import SockJS from 'sockjs-client';
+import {CompatClient, Stomp} from '@stomp/stompjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+interface Message {
+  username:string;
+  content:string;
 }
 
+const App = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [client, setClient] = useState<CompatClient | null>(null);
+  const [messageInput, setMessageInput] = useState('');
+  const [userName, setUserName] = useState('');
+
+
+  function deepCopy<Type>(obj : Type): Type{
+    return JSON.parse(JSON.stringify(obj));
+  }
+  const handleInputChange = (event : any) => {
+    setMessageInput(event.target.value);
+  };
+
+  const handleUsernameChange = (event: any) => {
+    setUserName(event.target.value);
+  }
+
+  const sendMessage = () => {
+    if (messageInput.trim() !== '') {
+      const message = {
+        content: messageInput,
+        username: userName,
+        password: 'root'
+      };
+
+      if (client) {
+        client.send("/topic/messages",{},JSON.stringify(message));
+      }
+
+      setMessageInput('');
+    }
+  };
+
+
+  useEffect(() => {
+    const stompClient = Stomp.over(() => {
+      return new SockJS('http://localhost:8080/websocket'); // Replace with your Spring Boot server URL
+    });
+
+    console.log("trying....")
+    stompClient.connect({}, (frame: any) => {
+      console.log('Connected to WebSocket');
+
+
+      stompClient.subscribe('/topic/messages', (message) => {
+        // console.log('Received message:', message.body);
+        const msg : Message = JSON.parse(message.body);
+        messages.push(msg);
+        const newMessages = deepCopy(messages);
+        setMessages(newMessages)
+        // Handle the received message in your React component
+      });
+    });
+
+    setClient(stompClient);
+
+    return () => {
+      stompClient.disconnect();
+      setClient(null);
+    };
+  }, []);
+  return (
+    <div>
+      <h1>Chat App</h1>
+      <input
+        type='text'
+        value={userName}
+        onChange={handleUsernameChange}
+        placeholder='Type your username...'
+      >
+      </input>
+      <br></br>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage();
+        }}
+      >
+        <input
+          type="text"
+          value={messageInput}
+          onChange={handleInputChange}
+          placeholder="Type your message..."
+        />
+        <button type="submit">Send</button>
+      </form>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>
+            <strong>{message.username}:</strong> {message.content}
+          </li>
+        ))}
+      </ul>
+     
+    </div>
+  );
+};
+
 export default App;
+
+
